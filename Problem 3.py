a1992b06d81468f890c547a310b0f8081ef27e8f
Josh Ward <38740500+j0shward@users.noreply.github.com>
2020-02-04 18:15:47 +0000

    Update Problem 3.py

--- Problem 3.py
+++ Problem 3.py
@@ -1,16 +1,19 @@
 import numpy as np
-import pandas as pd
+import pandas as pandas
 
 #problem 3
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
-def prime_factor(n):
-    q = []
-    for i in range(1,n):
-        if n % i == 0:
-            if all(i % j != 0 for j in range(2, i)):    
-                q.append(i)
-    return max(q)
+def max_prime_factor(n):
+    x = []
+    q = 2
+    while n > 1:
+        while n % q == 0:
+            x.append(q)
+            n = n/q
+        q = q + 1
 
-prime_factor(600851475143)
+    return max(x)
+
+max_prime_factor(600851475143)
