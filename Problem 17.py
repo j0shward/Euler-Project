36ba2f8f46f7bf15410665d2b9b2e346e2d7a18d
Josh Ward <38740500+j0shward@users.noreply.github.com>
2021-12-28 01:22:45 +0000

    Update Problem 17.py

--- Problem 17.py
+++ Problem 17.py
@@ -4,7 +4,6 @@
 
 # If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
 
-
 # Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
 import numpy as np
@@ -19,8 +18,11 @@ twos = {"1": "Ten", "2": "Twenty", "3": "Thirty", "4": "Forty",
 teens = {"10": "Ten", "11": "Eleven", "12": "Twelve", "13": "Thirteen", "14": "Fourteen",
          "15": "Fifteen", "16": "Sixteen", "17": "Seventeen", "18": "Eighteen", "19": "Nineteen"}
 
-length = 0
+h = "hundred"
+a = "and"
+t = "thousand"
 
+length = 0
 for x in range(1, 1001):
     x = str(x)
     if len(x) == 1:
@@ -32,31 +34,21 @@ for x in range(1, 1001):
             if x[1] == "0":
                 length += len(twos[x[0]])
             else:
-                length += len(twos[x[0]])
-                length += len(ones[x[1]])
+                length += len(twos[x[0]])+len(ones[x[1]])
     if len(x) == 3:
         if x[1:] == "00":
-            length += len(ones[x[0]])
-            length += len("hundred")
+            length += len(ones[x[0]])+len(h)
         else:
             if x[2] == "0":
-                length += len(ones[x[0]])
-                length += len("hundredand")
-                length += len(twos[x[1]])
+                length += len(ones[x[0]])+len(h)+len(a)+len(twos[x[1]])
             else:
                 if x[1] == "0":
-                    length += len(ones[x[0]])
-                    length += len("hundredand")
-                    length += len(ones[x[2]])
+                    length += len(ones[x[0]])+len(h)+len(a)+len(ones[x[2]])
                 elif x[1] == "1":
-                    length += len(ones[x[0]])
-                    length += len("hundredand")
-                    length += len(teens[x[1:]])
+                    length += len(ones[x[0]])+len(h)+len(a)+len(teens[x[1:]])
                 else:
-                    length += len(ones[x[0]])
-                    length += len("hundredand")
-                    length += len(twos[x[1]])
-                    length += len(ones[x[2]])
+                    length += len(ones[x[0]])+len(h)+len(a) + \
+                        len(twos[x[1]])+len(ones[x[2]])
     if len(x) == 4:
-        length += len("onethousand")
+        length += len(ones["1"])+len(t)
 print(length)
